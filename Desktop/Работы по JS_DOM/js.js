9fa9456045fbc649b74e8ed071fd29bfe7a36380
LEV <levkorsunovic@gmail.com>
2025-01-24 14:05:49 +0300

    remark js

--- Desktop/Работы по JS_DOM/js.js
+++ Desktop/Работы по JS_DOM/js.js
@@ -1,47 +1,41 @@
-
 function createRandomArray(length, min, max) {
   return Array.from(
     { length },
     () => Math.floor(Math.random() * (max - min + 1)) + min
   );
 }
+
 function createTableFromArray(array, rows, cols) {
   const table = document.createElement("table");
   let index = 0;
 
   for (let i = 0; i < rows; i++) {
     const row = document.createElement("tr");
-
     for (let j = 0; j < cols; j++) {
       const cell = document.createElement("td");
       const number = array[index++];
-
       cell.textContent = number;
       if (number >= 50) {
         cell.classList.add("orange");
       }
-
       row.appendChild(cell);
     }
-
     table.appendChild(row);
   }
-
   return table;
 }
+
 const randomNumbers = createRandomArray(30, 0, 99);
 const table = createTableFromArray(randomNumbers, 5, 6);
 document.getElementById("tableContainer").appendChild(table);
-const button = document.getElementById("clickedBtn");
-function addRandomCell() {
+
+function foo() {
   const randomNumber = Math.floor(Math.random() * 100);
   const newCell = document.createElement("td");
   newCell.textContent = randomNumber;
   if (randomNumber >= 50) {
     newCell.classList.add("orange");
   }
-
   const firstRow = table.querySelector("tr");
   firstRow.appendChild(newCell);
 }
-button.addEventListener("click", addRandomCell);
